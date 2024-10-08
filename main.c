bfcf44ac2e3df66321ba556175ce80492f040ba0
lucas <lucaszmasashi@gmail.com>
2024-10-07 22:27:17 -0300

    feita logica para iniciar o jogo caso o nome nao seja vazio

--- main.c
+++ main.c
@@ -5,14 +5,17 @@
 int main(void) {
     char nome[50];
 
+    //entrada
     printf("Digite seu nome: ");
     fgets(nome, 50, stdin); 
 
+    //tira o espaço
     int tamanho = strlen(nome);
     if (nome[tamanho - 1] == '\n') {
         nome[tamanho - 1] = '\0';
     }
 
+    //verifica se o nome é vazio ou não
     if (strlen(nome) > 0) {
         printf("Seu nome é: %s\n", nome);
         printf("Que comecem os jogos!!!!!!!\n");
