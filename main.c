8cd49e2e3b1ba979f80ae28b1f491b3f4437e6ea
lucas <lucaszmasashi@gmail.com>
2024-10-08 19:45:22 -0300

    feita logica para o usuario escolher aceitar ou nao o caso

--- main.c
+++ main.c
@@ -4,18 +4,30 @@
 
 int main(void) {
     char nome[50];
+    char resposta;
 
+    //entrada
     printf("Digite seu nome: ");
     fgets(nome, 50, stdin); 
 
+    //tira o espaço
     int tamanho = strlen(nome);
     if (nome[tamanho - 1] == '\n') {
         nome[tamanho - 1] = '\0';
     }
 
+    //verifica se o nome é vazio ou não
     if (strlen(nome) > 0) {
-        printf("Seu nome é: %s\n", nome);
-        printf("Que comecem os jogos!!!!!!!\n");
+        printf("Bem-vindo Detetive %s\n", nome);
+        printf("Tem um caso novo, deseja investigar: (S/N) \n");
+        scanf("%c", &resposta);
+        if (resposta == 'S' || resposta == 's'){
+            printf("caso aceito \n");
+        } else if (resposta == 'N' || resposta == 'n'){
+            printf("caso recusado \n");
+        } else {
+            printf("Resposta inválida \n");
+        }
     } else {
         printf("Nome não pode ser vazio!\n");
     }
