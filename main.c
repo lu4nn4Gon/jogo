3f2d2202b0bb3b283225e1558c71bf5ad9932880
luanna <luannagon1104@gmail.com>
2024-10-23 23:52:40 -0300

    feito workflow

--- main.c
+++ main.c
@@ -327,7 +327,7 @@ void Evidencias(Evidencia *evidencias, int numEvidencias) {
 
 void AcusarSuspeito(Suspeito *suspeitos, int numSuspeitos) {
     int opcaoSuspeito;
-    int evidenciasCorretas = 1;  // Aqui você pode ajustar a lógica de evidências se necessário.
+    int evidenciasCorretas = 1;  
 
     printf("\nEscolha o suspeito para acusar:\n");
     for (int i = 0; i < numSuspeitos; i++) {
@@ -347,18 +347,17 @@ void AcusarSuspeito(Suspeito *suspeitos, int numSuspeitos) {
 
     if (suspeitoEscolhido->culpado == 1 && evidenciasCorretas) {
         // Código ANSI para cor verde (acertou)
-        printf("\033[0;32m");  // Define a cor para verde
+        printf("\033[0;32m");  
         printf("\nParabéns! Você acusou corretamente %s.\n", suspeitoEscolhido->nome);
         printf("História resolvida: O irmão, enfurecido, atacou Hans Müller com um martelo e tentou encobrir o crime.\n");
         printf("Ele transferiu o dinheiro e tentou limpar as evidências na máquina de lavar.\n");
     } else {
         // Código ANSI para cor vermelha (errou)
-        printf("\033[0;31m");  // Define a cor para vermelho
+        printf("\033[0;31m");  
         printf("\nVocê acusou %s, mas esta não é a pessoa culpada.\n", suspeitoEscolhido->nome);
         printf("O verdadeiro culpado ainda está à solta.\n");
     }
 
-    // Reseta a cor para o padrão após a impressão
     printf("\033[0m");
 }
 
